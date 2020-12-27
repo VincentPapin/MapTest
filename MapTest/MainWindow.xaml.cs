137b65dda0ad692a0f335642a48e42befbe0327f
VincentPapin <vincent.papin@wanadoo.fr>
2020-12-27 16:42:22 +0100

    Gestion des vols : - récupération des vols - Affichage sur la carte sous forme de makers

--- MapTest/MainWindow.xaml.cs
+++ MapTest/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using GMap.NET;
 using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+using Newtonsoft.Json;
+
 
 namespace MapTest
 {
@@ -22,6 +26,9 @@ namespace MapTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string urlWS =
+            "http://api.aviationstack.com/v1/flights?access_key=5ac8339e195b4c8bb3fb500cd6c8f728";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -47,14 +54,66 @@ namespace MapTest
 
         private void BtnSearch_OnClick(object sender, RoutedEventArgs e)
         {
-            string saisieKeys = tbSearch.Text;
-            MapView_SetPosition(saisieKeys);
+            // string saisieKeys = tbSearch.Text;
+            // MapView_SetPosition(saisieKeys);
+            //Déclarartion du ListeVol
+            ListeVol listeVol = new ListeVol();
+
+            listeVol = GetListVoltWS(urlWS).Result;
+            AfficheVol(listeVol);
         }
 
         private void MapView_SetPosition(string keys)
         {
             mapView.SetPositionByKeywords(keys);
             mapView.Zoom = 12;
+        }
+
+        // public async void CallMethod()
+        // {
+        //     Task<ListeVol> task = GetListVoltWS(urlWS);
+        //     ListeVol listeVol = await task;
+        // }
+
+        static async Task<ListeVol> GetListVoltWS(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                ListeVol listeVol = new ListeVol();
+                string rawData = webClient.DownloadString(url);
+                listeVol = JsonConvert.DeserializeObject<ListeVol>(rawData);
+
+                return listeVol;
+            }
+        }
+
+        private void AfficheVol(ListeVol listeVol)
+        {
+            mapView.MapProvider = GMapProviders.GoogleMap;
+            mapView.Manager.Mode = AccessMode.ServerOnly;
+            mapView.SetPositionByKeywords("Paris, France");
+
+
+            foreach (Vol vol in listeVol.data)
+            {
+                if (vol.live is not null)
+                {
+                    var point = new PointLatLng(vol.live.latitude, vol.live.longitude);
+
+                    GMapMarker marker = new GMapMarker(point);
+
+                    marker.Shape = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Stroke = Brushes.Blue,
+                        StrokeThickness = 1.5
+                    };
+
+                    marker.ZIndex = 55;
+                    mapView.Markers.Add(marker);
+                }
+            }
         }
     }
 }
---
+++ MapTest/Vol.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MapTest
+{
+    public class VolLive
+    {
+        public double latitude{ get; set; }
+        public double longitude{ get; set; }
+        public double altitude{ get; set; }
+        public int direction{ get; set; }
+    }
+
+    public class Vol
+    {
+        public DateTime flight_date{ get; set; }
+        public string flight_status { get; set; }
+        public VolLive live { get; set; }
+    }
+
+    public class ListeVol
+    {
+        public List<Vol> data { get; set; } = new List<Vol>();
+    }
+}
